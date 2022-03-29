658f1f111e5a151668460c8d571fc7785e136a17
mhdirajabi <mehdirajabi@disroot.org>
2022-03-29 22:01:01 +0430

    Tweaks and configurations to dockerfile and docker compose.

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,27 @@
+# pull official base image
 FROM python:3.7-alpine
-LABEL org.opencontainers.image.authors="mehdirajabi@disroot.org"
 
-ENV PYTHONUNBUFFERED 1
-
-COPY ./requirements.txt /requirements.txt
-RUN pip install -r /requirements.txt
-
+# set working directory
 RUN mkdir /app
 WORKDIR /app
 COPY ./app /app
 
-RUN adduser -D user
-USER user
+# set environment variables
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+# install psycopg2 dependencies
+RUN apk add --no-cache postgresql14-dev=~14.2-r0 gcc=~10.3.1_git20211027-r0 \
+    python3-dev=~3.9.7-r4 musl-dev=~1.2.2-r7 \
+# install dependencies
+    && pip install --upgrade --no-cache-dir pip==22.0.4
+COPY ./requirements.txt /requirements.txt
+RUN pip install --no-cache-dir -r /requirements.txt
+
+# copy entrypoint.sh
+COPY ./entrypoint.sh /entrypoint.sh
+RUN sed -i 's/\r$//g' /entrypoint.sh \
+    && chmod +x /entrypoint.sh
+
+# run entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
--- app/app/settings.py
+++ app/app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-szddzg(^^d5+b+ymcy!a5xyh3z2*o3_0arrv64)&od%a%mcn+r"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -76,8 +77,12 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.environ.get("ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("DB_NAME", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("USER", "user"),
+        "PASSWORD": os.environ.get("PASSWORD", "password"),
+        "HOST": os.environ.get("HOST", "localhost"),
+        "PORT": os.environ.get("PORT", "5432"),
     }
 }
 
